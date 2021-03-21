fb749fc12614ab8b6148861caac509b436cd4c97
Sergey Galichanin <sergerg@gmail.com>
2021-03-21 20:18:52 +0400

    H2 in pom

--- src/test/java/serger/webf/WebfApplicationTests.java
+++ src/test/java/serger/webf/WebfApplicationTests.java
@@ -1,11 +1,13 @@
 package serger.webf;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class WebfApplicationTests {
 
+    //@Disabled
     @Test
     void contextLoads() {
     }
