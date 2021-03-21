46767ba5ab2b0b28315da310f5031d8f324f483a
Sergey Galichanin <sergerg@gmail.com>
2021-03-21 20:42:45 +0400

    Тесты - добавил класс определения бинов.

--- src/test/java/serger/webf/WebfApplicationTests.java
+++ src/test/java/serger/webf/WebfApplicationTests.java
@@ -3,8 +3,10 @@ package serger.webf;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration(initializers = WebfApplication.class)
 class WebfApplicationTests {
 
     //@Disabled
