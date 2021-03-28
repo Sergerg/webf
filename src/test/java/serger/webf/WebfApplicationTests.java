3544a10c4294d5251d9f593f0fe2cce4ec6ba04f
Sergey Galichanin <sergerg@gmail.com>
2021-03-28 20:03:48 +0400

    Тест контроллера

--- src/test/java/serger/webf/WebfApplicationTests.java
+++ src/test/java/serger/webf/WebfApplicationTests.java
@@ -2,16 +2,40 @@ package serger.webf;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
 
 @SpringBootTest
 @ContextConfiguration(initializers = WebfApplication.class)
+@AutoConfigureWebTestClient
 class WebfApplicationTests {
+
+    @Autowired
+    private WebTestClient client;
 
     //@Disabled
     @Test
     void contextLoads() {
+        ;
     }
+
+    @Test
+    public void theAnswer() throws Exception {
+        client.get().uri("/the-answer")//.body(Mono.just("par1"), String.class)
+                .exchange().expectStatus().isOk()
+                .expectBody(Integer.class).isEqualTo(42);
+    }
+
+    @Test
+    public void helloWorld() throws Exception {
+        client.get().uri("/hello-world")//.body(Mono.just("par1"), String.class)
+                .exchange().expectStatus().isOk()
+                .expectBody(String.class).isEqualTo("Hello, null!");
+    }
+
 
 }
